ea8c1ab75a154b3770d239579f19aff573920e71
Robert Dopieralski <abc507446692@gmail.com>
2019-03-29 20:06:42 +0100

    WIP: table with items added

--- src/Components/Items/Items.js
+++ src/Components/Items/Items.js
@@ -1,17 +1,54 @@
 import React, { Component } from "react";
+import { Table, Checkbox } from "semantic-ui-react";
 
 class Items extends Component {
   state = {
     products: []
   };
 
+  componentDidMount() {
+    fetch(process.env.PUBLIC_URL + "/data/products.json").then(response =>
+      response
+        .json()
+        .then(data => this.setState({ products: data.products }))
+        .catch(error => console.log(error))
+    );
+  }
+
   render() {
+    const { products } = this.state;
     return (
-      <div>
-        <p>Zupa</p>
+      <div className="products-list">
+        <Table celled padded>
+          <Table.Header>
+            <Table.Row>
+              <Table.HeaderCell>Product Name</Table.HeaderCell>
+              <Table.HeaderCell>Description</Table.HeaderCell>
+              <Table.HeaderCell>Price</Table.HeaderCell>
+              <Table.HeaderCell>Free Shipping</Table.HeaderCell>
+            </Table.Row>
+          </Table.Header>
+
+          <Table.Body>
+            {products.length
+              ? products.map(prod => <Product item={prod} />)
+              : ""}
+          </Table.Body>
+        </Table>
       </div>
     );
   }
 }
+
+const Product = ({ item }) => (
+  <Table.Row>
+    <Table.Cell>{item.title}</Table.Cell>
+    <Table.Cell>{item.description}</Table.Cell>
+    <Table.Cell>{`${item.price} ${item.currencyFormat}`}</Table.Cell>
+    <Table.Cell>
+      <Checkbox disabled checked={item.isFreeShipping} />
+    </Table.Cell>
+  </Table.Row>
+);
 
 export default Items;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
+
 import Root from "./Components/Root";
+
+import "./index.css";
+import "semantic-ui-css/semantic.min.css";
+
 import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(<Root />, document.getElementById("root"));
